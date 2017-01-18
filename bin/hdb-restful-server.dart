6cf2c938609d7a97a20158df75a82fdf0114149a
Åsmund Stavdahl <smund3@gmail.com>
2017-01-18 22:27:43 +0100

    functional hdb implementation (no persistence)

--- bin/hdb-restful-server.dart
+++ bin/hdb-restful-server.dart
@@ -1,1 +1,88 @@
-library hdb_restfull;
+import 'dart:io';
+import "dart:convert" show UTF8;
+
+Map<String, String> data = new Map<String, String>();
+
+main() async {
+  var requestServer =
+      await HttpServer.bind(InternetAddress.ANY_IP_V4, 44550);
+  print('listening on localhost, port ${requestServer.port}');
+
+  await for (HttpRequest request in requestServer) {
+    print(request.method + " " + request.uri.toString());
+
+    String key = request.uri.toString();
+    key = key.replaceAll(new RegExp('//+'), "/");
+    String value = await UTF8.decodeStream(request);
+
+    switch(request.method) {
+      case "GET":
+        String result;
+        if(key.endsWith("/")){
+          result = getChildrenAtKey(key).join("\n");
+        } else {
+          result = getData(key);
+        }
+        request.response.write(result);
+        request.response.close();
+        break;
+      case "PUT":
+        setData(key, value);
+        request.response.write("");
+        request.response.close();
+        break;
+      case "POST":
+        int index = 1 + getLastNumberedChildIndexAtKey(key);
+        String childKey = key+"/${index}";
+        setData(childKey, value);
+        request.response.write("${index}");
+        request.response.close();
+        break;
+      default:
+        request.response.write("Unsupported method.");
+        request.response.close();
+    }
+  }
+}
+
+int getLastNumberedChildIndexAtKey(String parentKey){
+  int lastNumberedChildIndex = 0;
+
+  String childIndex;
+  data.forEach((key, value) {
+    if(key.startsWith(parentKey) && key != parentKey){
+      childIndex = key.split("/").last;
+      try {
+        var childNumberedIndex = int.parse(childIndex);
+        if(childNumberedIndex > lastNumberedChildIndex){
+          lastNumberedChildIndex = childNumberedIndex;
+        }
+      } catch(exception){
+        //print(exception);
+      }
+    }
+  });
+
+  return lastNumberedChildIndex;
+}
+
+String getData(key){
+  return data[key];
+}
+
+void setData(key, value){
+  print("\t" + key + " ← " + value);
+  data[key] = value;
+}
+
+List<String> getChildrenAtKey(parentKey){
+  List<String> children = new List<String>();
+
+  data.forEach((key, value) {
+    if(key.startsWith(parentKey) && key != parentKey){
+      children.add(key.split("/").last);
+    }
+  });
+
+  return children;
+}
--- test/end-user-tests.sh
+++ test/end-user-tests.sh
@@ -35,11 +35,13 @@ Ok?(){
 
 require curl grep
 
-HDB_HOST=localhost:80 #44550
+HDB_HOST=localhost:44550
+
 
 Test "can get any value from base path"
 curl --silent --include $HDB_HOST | head -n1 | grep "HTTP/.\.. 200 OK"
 Ok?
+
 
 Test "can get set a value to 'z' key"
 value="shallabais"
@@ -47,10 +49,12 @@ o=$(curl -X PUT -d "$value" --silent --include $HDB_HOST/z)
 echo "$o" | head -n1 | grep "HTTP/.\.. 200 OK"
 Ok? || echo "response: $o"
 
+
 Test "can get the new value of the 'z' key"
 retrievedValue=$(curl -X GET --silent $HDB_HOST/z)
 [ "$retrievedValue" == "$value" ]
 Ok? || echo "retrievedValue: $retrievedValue"
+
 
 Test "can change the value of the 'z' key"
 newValue="SHAZZAMM! hahahah..,ads,o2kq09 j=(JHRE)#(HR)=#HD ;ØASL;łĸ µª©œπµn}£"
@@ -58,3 +62,44 @@ curl -X PUT -d "$newValue" --silent $HDB_HOST/z >/dev/null
 retrievedNewValue=$(curl -X GET --silent $HDB_HOST/z)
 [ "$retrievedNewValue" == "$newValue" ]
 Ok? || echo "retrievedNewValue: $retrievedNewValue"
+
+
+Test "gets an incrementing numbered index back when appending a children"
+retrievedIndex1=$(curl -X POST -d "v1" --silent $HDB_HOST/z)
+retrievedIndex2=$(curl -X POST -d "v2" --silent $HDB_HOST/z)
+[ 1$retrievedIndex1 -lt 1$retrievedIndex2 ]
+Ok? || echo "retrievedIndex1: $retrievedIndex1, retrievedIndex2: $retrievedIndex2"
+
+
+Test "can append automatically children"
+newValue1="value1"
+newValue2="value2"
+retrievedIndex=$(curl -X POST -d "$newValue1" --silent $HDB_HOST/z)
+curl -X POST -d "$newValue2" --silent $HDB_HOST/z >/dev/null
+retrievedNewValue2=$(curl -X GET --silent $HDB_HOST/z/$(echo "1 + $retrievedIndex" | bc))
+[ "$retrievedNewValue2" == "$newValue2" ]
+Ok? || echo "retrievedNewValue: $retrievedNewValue"
+
+
+Test "indexes of appended children starts at 1"
+key=$(curl -X POST -d "$newValue1" --silent $HDB_HOST/z)
+retrievedIndex=$(curl -X POST -d "v" --silent $HDB_HOST/z/$key)
+[ "x$retrievedIndex" == "x1" ]
+Ok? || echo "retrievedIndex: $retrievedIndex"
+
+
+Test "can get a list of children of corrent length"
+key=$(curl -X POST -d "$newValue1" --silent $HDB_HOST/z)
+numberOfChildren=10
+for n in $(seq 1 $numberOfChildren); do
+    curl -X POST -d "$newValue2" --silent $HDB_HOST/z/$key >/dev/null
+done
+listOfChildren=$(curl -X GET --silent $HDB_HOST/z/$key/)
+numberOfChildren=$(echo "$listOfChildren" | wc -l)
+[ $numberOfChildren -eq 10 ]
+Ok? || echo -e "numberOfChildren: $numberOfChildren"
+
+Test "can get a list of children of corrent format"
+echo "$listOfChildren" | grep -F "/" >/dev/null
+[ $? -ne 0 ]
+Ok? || echo -e "numberOfChildren: $numberOfChildren, listOfChildren:\n$listOfChildren"
